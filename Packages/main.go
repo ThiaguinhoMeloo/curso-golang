40469bc362bf99954050185496a4f1a2541750bc
Thiago Melo <thiagomelo@itstransdata.com>
2024-08-22 14:35:03 -0300

    feat: Using external package

--- Packages/main.go
+++ Packages/main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"fmt"
 	"module/assistant"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo do arquivo main")
 	assistant.ToWrite()
+
+	err := checkmail.ValidateFormat("123456789")
+	if err != nil {
+		fmt.Println(err)
+	}
 }
