c21bd72414a926c09a180e437c6edb90a807875d
Thiago Melo <thiagomelo@itstransdata.com>
2024-08-22 14:25:29 -0300

    feat: Add Packages

---
+++ Packages/assistant/assistant.go
@@ -0,0 +1,11 @@
+package assistant
+
+import (
+	"fmt"
+)
+
+// ToWrite registra uma mensagem na tela
+func ToWrite() {
+	fmt.Println("Escrevendo do pacote auxiliar")
+	towrite()
+}
---
+++ Packages/assistant/assistant2.go
@@ -0,0 +1,10 @@
+package assistant
+
+import(
+	"fmt"
+)
+
+// towrite registra uma mensagem na tela
+func towrite(){
+	fmt.Println("Escrevendo pela função escrevendo 2!")
+}
